941b27904733e7de957c46e767518b42db6cddb1
park-eunseop <71587746+park-eunseop@users.noreply.github.com>
2022-01-25 21:23:48 +0900

    edit HttpController.java

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 요청 -> 응답(html)    @Controller
@@ -15,17 +17,18 @@ public class HttpControllerTest {
 	//인터넷 브라우저 요청은 무조건 get요청만 할 수 있어
 	//http://localhost:8080/http/get
 	@GetMapping("/http/get")
-	public String getTest() {
-		return "get 요청";
+	public String getTest(Member m) {
+		return "get 요청"+m.getId()+","+m.getUsername();
 	}
 	
 	@PostMapping("/http/post")
-	public String postTest() {
-		return "post 요청";
+	public String postTest(@RequestBody Member m) {
+		
+		return "post 요청"+m.getId()+","+m.getUsername();
 	}
 	
 	@PutMapping("/http/put")
-	public String putTest() {
+	public String putTest(@RequestBody Member m) {
 		return "put 요청";
 	}
 	
